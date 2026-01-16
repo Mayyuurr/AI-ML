4c80e81171457c540c319e95bae3870cb7540ce3
Mayur <mayurrathodavni@gmail.com>
2026-01-16 15:31:10 +0530

    Python Basics

---
+++ python_basics/01_basics/hello_python.py
@@ -0,0 +1,18 @@
+print("Hello world")
+
+def chai(n):
+    print(n)
+
+chai(4)
+
+chai_one="lemon tea"
+chai_two="ginger tea"
+chai_three="masala tea"
+
+# in python mutuable data structure mean that wont be changed 
+# for ex if created a string "chai" and then chanegd to chai and code so the var is assigned to new string and the original stays there 
+# every data structure in python is treadted as object
+
+#from importlib import reload
+#>>> reload(hello_python)
+# this is used to reload a imported module
